c8984a6cfa0000edc604593f9021da280e2a09bd
Kapeland <mdidkovskij@gmail.com>
2024-01-22 21:39:13 +0300

    Changed ret codes, CreateWalletHandler now creates wallet and sends proper response.

--- cmd/httpServer/internal/handlers/reqHandlers.go
+++ cmd/httpServer/internal/handlers/reqHandlers.go
@@ -1,19 +1,28 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
+	wlt "infotecsSelection/cmd/httpServer/internal/wallet"
+	intl "infotecsSelection/internal"
+	"log"
 	"net/http"
 )
 
 func CreateWalletHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "POST":
-		_, err := fmt.Fprint(w, "Create wallet.")
+		tmpWlt := wlt.CreateWallet(intl.GetUUID())
+		jsonData, err := json.Marshal(tmpWlt)
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.Write(jsonData)
+
 		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
+			w.WriteHeader(http.StatusBadRequest)
+			log.Fatal(err)
 		}
 	default:
-		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.WriteHeader(http.StatusBadRequest)
 	}
 }
 func WalletInfoHandler(w http.ResponseWriter, r *http.Request) {
@@ -24,6 +33,6 @@ func WalletInfoHandler(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusInternalServerError)
 		}
 	default:
-		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.WriteHeader(http.StatusBadRequest)
 	}
 }
